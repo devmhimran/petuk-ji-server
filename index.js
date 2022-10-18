be690037cd3c96180dda832b59a282f2cc2b0d80
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-10-18 11:38:31 +0600

    Added Required Files

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+var cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+require('dotenv').config()
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
